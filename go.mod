0ecb48992e2a4a08c5d55b365dd2114e561b0433
Yahabiohim <yahapmi+biohim@gmail.com>
2024-02-27 14:41:11 +0300

    WIP go mod tutorial

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module hello
+module github.com/hexlet/hello-hexlet
 
 go 1.20
+
+require github.com/sirupsen/logrus v1.5.0
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"hello/greeting"
+// )
+
+// func main() {
+// 	fmt.Println(greeting.Hello())
+// }
+
 package main
 
-import (
-	"fmt"
-	"hello/greeting"
-)
+import "github.com/sirupsen/logrus"
 
 func main() {
-	fmt.Println(greeting.Hello())
+	logrus.Println("Hello, Hexlet!")
 }
